33948c288dbc0f6f473b9cb513e2166ea2e5d55e
asami <vbo.secondary@gmail.com>
2022-11-03 14:19:42 +0100

    commit with todo

--- src/main/java/Added.java
+++ src/main/java/Added.java
@@ -2,6 +2,22 @@ public class Added {
 
     public static void main(String[] args) {
 
+
+        //IntelliJ IDEA 2023.1 EAP (Ultimate Edition)
+        //Build #IU-231.725, built on November 2, 2022
+        //Licensed to IntelliJ IDEA EAP user: Victoria Bozhko
+        //Expiration date: December 2, 2022
+        //Runtime version: 17.0.5+8-b681.33 amd64
+        //VM: OpenJDK 64-Bit Server VM by JetBrains s.r.o.
+        //Linux 5.15.0-48-generic
+        //GC: G1 Young Generation, G1 Old Generation
+        //Memory: 994M
+        //Cores: 2
+        //Non-Bundled Plugins:
+        //    com.intellij.grazie.pro (0.3.110-223)
+        //
+        //Kotlin: 231-1.7.20-release-201-IJ725
+        //Current Desktop: ubuntu:GNOME
     }
     //change
     //todo this
